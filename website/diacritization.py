2e9364dd5d5c6d9eca070d447419aa9d679b11fc
Mohamed Eldesouki <mohamohamed@qf.org.qa>
2019-07-07 15:41:34 +0300

    Fix bug when voting

--- website/diacritization.py
+++ website/diacritization.py
@@ -2,17 +2,18 @@ import codecs
 from . import ca_runner, msa_runner, tn_runner, ma_runner
 from random import randint
 import pyarabic.araby as araby
+from pyarabic.araby import strip_tatweel
 from collections import defaultdict, Counter
 
 
 class LastNTokens(object):
-    def __init__(self, n):
+    def __init__(self, n, sos):
         # last number of token to be reserved
         self.n = n
         self.session = 0
         self.tokens = list()
         for i in range(n):
-            self.tokens.append('بدايةجملة')
+            self.tokens.append(sos)
 
     def get_n_tokens(self):
         # return self.tokens[-self.n:]
@@ -29,33 +30,38 @@ class LastNTokens(object):
 
 
 def run_diac(gomla, dialect):
-    token_list_7 = LastNTokens(7)
+    sos = 'بدايةجملة' if dialect == 'ca' else 'بداية'
+    eos = 'نهايةجملة' if dialect == 'ca' else 'نهاية'
+    token_list_7 = LastNTokens(7, sos)
+
     fname = randint(0, 100000)
     with codecs.open(f'diacritizer/userdata/{dialect}/{fname}.fmt', mode='w', encoding='utf-8') as infile:
-        gomla = araby.normalize_ligature(gomla)
-        gomla_list = araby.tokenize(gomla.replace('_', ''), conditions=araby.is_arabicrange, morphs=araby.strip_tashkeel)
+        gomla = strip_tatweel(araby.normalize_ligature(gomla))
+        # gomla_list = araby.tokenize(gomla.replace('_', '-'), conditions=araby.is_arabicrange, morphs=araby.strip_tashkeel)
+        gomla_list = araby.tokenize(gomla.replace('_', '-'), morphs=araby.strip_tashkeel)
+        # gomla_list = gomla.strip().split()
 
         for token in gomla_list:
             t = ' '.join(token)
             token_list_7.add_tokens_list(t, 0)
             infile.write(token_list_7.get_n_tokens() + '\n')
         else:
-            token_list_7.add_tokens_list('نهايةجملة', 0)
+            token_list_7.add_tokens_list(eos, 0)
             infile.write(token_list_7.get_n_tokens() + '\n')
 
-            token_list_7.add_tokens_list('نهايةجملة', 0)
+            token_list_7.add_tokens_list(eos, 0)
             infile.write(token_list_7.get_n_tokens() + '\n')
 
-            token_list_7.add_tokens_list('نهايةجملة', 0)
+            token_list_7.add_tokens_list(eos, 0)
             infile.write(token_list_7.get_n_tokens() + '\n')
 
-            token_list_7.add_tokens_list('نهايةجملة', 0)
+            token_list_7.add_tokens_list(eos, 0)
             infile.write(token_list_7.get_n_tokens() + '\n')
 
-            token_list_7.add_tokens_list('نهايةجملة', 0)
+            token_list_7.add_tokens_list(eos, 0)
             infile.write(token_list_7.get_n_tokens() + '\n')
 
-            token_list_7.add_tokens_list('نهايةجملة', 0)
+            token_list_7.add_tokens_list(eos, 0)
             infile.write(token_list_7.get_n_tokens() + '\n')
 
     if dialect == 'ca':
@@ -80,7 +86,7 @@ def run_diac(gomla, dialect):
             for j, _ in enumerate(dtokens):
                 tk = dtokens[j - 1 - i % 7]
 
-                if tk not in ['نهايةجملة', 'بدايةجملة']:
+                if tk not in [eos, sos]:
                     counters[j].update([tk])
 
                 if sum(counters[j].values()) >= 7:
